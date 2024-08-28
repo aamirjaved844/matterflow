7c1225fd043a16bc80318cebdc601152ca1045ec
Ivo Brett <oideibrett@lccgmail.com>
2024-08-28 17:06:54 +0100

    added homeassistant as allowed host

--- api/mf/mf/settings.py
+++ api/mf/mf/settings.py
@@ -30,7 +30,8 @@ ALLOWED_HOSTS = ['back-end:8000',
                  'localhost',
                  '127.0.0.1',
                  '0.0.0.0',
-                 '[::1]']
+                 '[::1]',
+                 'homeassistant.local']
 
 
 # Application definition
