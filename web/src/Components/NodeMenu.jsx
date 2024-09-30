96cdf7077afb6f80a55d6425686bd58e4d3e127e
Aamir Javed <aamirjaved844@gmail.com>
2024-09-30 10:00:44 +0500

    Updated UI of node menu

--- web/src/Components/NodeMenu.jsx
+++ web/src/Components/NodeMenu.jsx
@@ -1,37 +1,54 @@
-import React from 'react';
-import * as _ from 'lodash';
-import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+import React from "react";
+import * as _ from "lodash";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
 import CustomNodeUpload from "./CustomNodeUpload";
-
+import { Collapse } from "antd";
 
 export default function NodeMenu(props) {
-    // construct menu from JSON of node types
-    return (
-        <div className="NodeMenu">
-            <h3>Node Menu</h3>
-            <div>Drag-and-drop nodes to build a data workflow.</div>
-            <hr />
-            {_.map(props.nodes, (items, section) =>
-                <div key={`node-menu-${section}`}>
-                    <span className="node-section-title">{section}</span>
-                    <ul>
-                        { _.map(items, item => {
-                            const data = {...item}; // copy so we can mutate
-                            const config = data.options;
-                            delete data.options;
-                            return (
-                                <NodeMenuItem key={data.node_key || data.filename}
-                                              nodeInfo={data} config={config} />
-                            )}
-                        )}
-                    </ul>
-                </div>
-            )}
-            <CustomNodeUpload onUpload={props.onUpload} />
-       </div>
-    );
-}
+  // construct menu from JSON of node types
 
+  const menuItems = _.map(props.nodes, (items, section) => {
+    return {
+      key: section,
+      label: section,
+      children: (
+        <ul style={{ marginBottom: 0 }}>
+          {_.map(items, (item) => {
+            const data = { ...item }; // copy so we can mutate
+            const config = data.options;
+            delete data.options;
+            return (
+              <NodeMenuItem
+                key={data.node_key || data.filename}
+                nodeInfo={data}
+                config={config}
+              />
+            );
+          })}
+          {section === "Custom Nodes" ? (
+            <CustomNodeUpload onUpload={props.onUpload} />
+          ) : (
+            <></>
+          )}
+        </ul>
+      ),
+    };
+  });
+
+  return (
+    <div className="NodeMenu">
+      <h3>Node Menu</h3>
+      <div>Drag-and-drop nodes to build a data workflow.</div>
+      <Collapse
+        accordion
+        items={menuItems}
+        bordered={false}
+        ghost
+        style={{ margin: "16px -16px" }}
+      />
+    </div>
+  );
+}
 
 /**
  * Format docstring with newlines into tooltip content
@@ -39,61 +56,64 @@ export default function NodeMenu(props) {
  * @returns {array} - array of strings and HTML elements
  */
 function formatTooltip(string) {
-    const split = string.split("\n");
-    const out = [];
-    split.forEach((line, i) => {
-        out.push(line);
-        out.push(<br  key={i} />);
-    });
-    out.pop();
-    return out;
+  const split = string.split("\n");
+  const out = [];
+  split.forEach((line, i) => {
+    out.push(line);
+    out.push(<br key={i} />);
+  });
+  out.pop();
+  return out;
 }
-
 
 function NodeMenuItem(props) {
-    if (!props.nodeInfo.missing_packages) {
-        const tooltip = props.nodeInfo.doc ? formatTooltip(props.nodeInfo.doc) : "This node has no documentation."
-        return (
-            <OverlayTrigger
-                placement="left"
-                delay={{ show: 250, hide: 250 }}
-                overlay={<NodeTooltip message={tooltip} />}>
-                <li className="NodeMenuItem"
-                    draggable={true}
-                    onDragStart={event => {
-                        event.dataTransfer.setData(
-                            'storm-diagram-node',
-                            JSON.stringify(props));
-                    }}
-                    style={{color: props.nodeInfo.color}}>
-                    {props.nodeInfo.name}
-                </li>
-            </OverlayTrigger>
-        )
-    } else {
-        let tooltip = "These Python modules could not be imported:\n\n"
-            + props.nodeInfo.missing_packages.join("\n");
-        tooltip = formatTooltip(tooltip);
-        return (
-            <OverlayTrigger
-                placement="left"
-                delay={{ show: 250, hide: 250 }}
-                overlay={<NodeTooltip message={tooltip} />}>
-                <li className="NodeMenuItem invalid">{props.nodeInfo.filename}</li>
-            </OverlayTrigger>
-        )
-    }
+  if (!props.nodeInfo.missing_packages) {
+    const tooltip = props.nodeInfo.doc
+      ? formatTooltip(props.nodeInfo.doc)
+      : "This node has no documentation.";
+    return (
+      <OverlayTrigger
+        placement="left"
+        delay={{ show: 250, hide: 250 }}
+        overlay={<NodeTooltip message={tooltip} />}
+      >
+        <li
+          className="NodeMenuItem"
+          draggable={true}
+          onDragStart={(event) => {
+            event.dataTransfer.setData(
+              "storm-diagram-node",
+              JSON.stringify(props)
+            );
+          }}
+          style={{ color: props.nodeInfo.color, borderRadius: 6 }}
+        >
+          {props.nodeInfo.name}
+        </li>
+      </OverlayTrigger>
+    );
+  } else {
+    let tooltip =
+      "These Python modules could not be imported:\n\n" +
+      props.nodeInfo.missing_packages.join("\n");
+    tooltip = formatTooltip(tooltip);
+    return (
+      <OverlayTrigger
+        placement="left"
+        delay={{ show: 250, hide: 250 }}
+        overlay={<NodeTooltip message={tooltip} />}
+      >
+        <li className="NodeMenuItem invalid">{props.nodeInfo.filename}</li>
+      </OverlayTrigger>
+    );
+  }
 }
-
 
 // Overlay with props has to use ref forwarding
 const NodeTooltip = React.forwardRef((props, ref) => {
-    return (
-        <Tooltip {...props} ref={ref}>
-            <div style={{textAlign: "left"}}>
-                {props.message}
-            </div>
-        </Tooltip>
-    )
+  return (
+    <Tooltip {...props} ref={ref}>
+      <div style={{ textAlign: "left" }}>{props.message}</div>
+    </Tooltip>
+  );
 });
-
