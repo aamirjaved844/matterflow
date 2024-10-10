58079e339c94d876875afebef2dcb66e857f8d58
Aamir Javed <aamirjaved844@gmail.com>
2024-10-10 14:20:31 +0500

    Left side menu updated (flow menu and modeling menu)

--- web/src/Components/ModelMenu.jsx
+++ web/src/Components/ModelMenu.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
-import ModelsInstancesList from './ModelsInstancesList'
+import React from "react";
+import ModelsInstancesList from "./ModelsInstancesList";
+import { Card, Typography } from "antd";
 
 const ModelMenu = () => {
   return (
-    <div className="FlowMenu" style={{ paddingTop: '20px' }}>
-        <h3>Modelling Menu</h3>
-        <div>Define your data models and instances.</div>
-        <hr />
-        <ModelsInstancesList/>    
-    </div>
-  )
-}
+    <Card style={{ marginTop: 24 }}>
+      <div style={{ margin: -16 }}>
+        <Typography.Title level={3}>Modelling Menu</Typography.Title>
+        <Typography.Text type="secondary">
+          Define your data models and instances.
+        </Typography.Text>
+        <div className="FlowMenu" style={{ paddingTop: "20px" }}>
+          <ModelsInstancesList />
+        </div>
+      </div>
+    </Card>
+  );
+};
 
-export default ModelMenu
+export default ModelMenu;
